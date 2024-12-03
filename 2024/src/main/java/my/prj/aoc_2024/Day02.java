99f799f893951763473ecb4987f7372fad6f32fa
alen-mat <26516117+alen-mat@users.noreply.github.com>
2024-12-03 23:30:05 +0530

    2024 : day2 & 3

---
+++ 2024/src/main/java/my/prj/aoc_2024/Day02.java
@@ -0,0 +1,84 @@
+package my.prj.aoc_2024;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Day02
+ * --- Day 2: Red-Nosed Reports ---
+ * `https://adventofcode.com/2024/day/2`
+ */
+public class Day02 implements ISolution {
+    Object[] isSafe(List<Integer> vals) {
+        int i = 0, j = i + 1;
+        Boolean shouldBePos = null;
+        while (j < vals.size()) {
+            int diff = vals.get(i) - vals.get(j);
+            boolean isPositive = diff > 0;
+            shouldBePos = (shouldBePos == null) ? isPositive : shouldBePos;
+
+            if (shouldBePos != isPositive) {
+                return new Object[] { false, j };
+            }
+
+            diff = Math.abs(diff);
+            if (!(diff >= 1 && diff <= 3)) {
+                return new Object[] { false, j };
+            }
+            i = j;
+            j += 1;
+        }
+        return new Object[] { true, null };
+    }
+
+    @Override
+    public void solve() {
+        int safeCount = 0;
+        int safeCountD = 0;
+        String input = getInput();
+        String[] lines = input.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            List<Integer> vals = Arrays.stream(lines[i].split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+            Object[] safe = isSafe(vals);
+            if ((boolean) safe[0]) {
+                safeCount++;
+                safeCountD++;
+            } else {
+                int j = (int) safe[1];
+                AtomicBoolean ok = new AtomicBoolean(false);
+                Function<Integer, Void> consider = (index) -> {
+                    ArrayList<Integer> val_cp = new ArrayList<>(vals);
+                    val_cp.remove((int) index);
+                    if ((boolean) isSafe(val_cp)[0]) {
+                        ok.set(true);
+                    }
+                    return null;
+                };
+
+                consider.apply(j);
+                if (!ok.get() && j + 1 < vals.size() - 1) {
+                    consider.apply(j + 1);
+                }
+                if (!ok.get() && j + 2 < vals.size() - 1) {
+                    consider.apply(j + 2);
+                }
+                if (!ok.get()) {
+                    consider.apply(0);
+                }
+                if (!ok.get()) {
+                    consider.apply(vals.size() - 1);
+
+                }
+                if (ok.get()) {
+                    safeCountD++;
+                }
+            }
+        }
+
+        displayVal(safeCount + "", safeCountD + "");
+    }
+}
---
+++ 2024/src/main/java/my/prj/aoc_2024/Day03.java
@@ -0,0 +1,55 @@
+package my.prj.aoc_2024;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Day03
+ */
+public class Day03 implements ISolution {
+    final String PATTERN1 = ".*?mul\\((\\d+),(\\d+)\\).*?";
+    final Pattern p1 = Pattern.compile(PATTERN1);
+
+    final String PATTERN2 = "(mul\\((\\d{1,3}),(\\d{1,3})\\))|(do\\(\\))|(don't\\(\\))";
+    final Pattern p2 = Pattern.compile(PATTERN2);
+
+    long total1 = 0;
+    long total2 = 0;
+
+    public void part1() {
+        long total = 0;
+        String input = getInput();
+        Matcher m1 = p1.matcher(input);
+        while (m1.find()) {
+            total = (Integer.parseInt(m1.group(1)) * Integer.parseInt(m1.group(2))) + total;
+        }
+
+        this.total1 = total;
+    }
+
+    public void part2() {
+        long total = 0;
+        String input = getInput();
+        boolean d_o = true;
+        Matcher m2 = p2.matcher(input);
+        while (m2.find()) {
+            if (m2.group(4) != null) {
+                d_o = true;
+            }
+            else if (m2.group(5) != null) {
+                d_o = false;
+            }
+            else if (d_o && m2.group(1) != null) {
+                total += Integer.parseInt(m2.group(2)) * Integer.parseInt(m2.group(3));
+            }
+        }
+        this.total2 = total;
+    }
+
+    @Override
+    public void solve() {
+        part1();
+        part2();
+        displayVal(total1 + "", total2 + "");
+    }
+}
